76406be49e7a0f2162c7c15148226f0b9cd575a9
G00389482@atu.ie <G00389482@atu.ie>
2024-10-21 13:05:38 +0100

    enrolment class set up

--- src/main/java/atu/ie/enrolmentservice/Enrolment.java
+++ src/main/java/atu/ie/enrolmentservice/Enrolment.java
@@ -1,4 +1,9 @@
 package atu.ie.enrolmentservice;
 
 public class Enrolment {
+    private long enrolmentId;
+    private long studentId;
+    private String studentName;
+    private String courseCode;
+    private String enrollmentDate;
 }
