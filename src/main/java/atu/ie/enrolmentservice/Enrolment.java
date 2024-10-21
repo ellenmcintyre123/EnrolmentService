dbc4066d7b162566ec549e4db8e63ad056846981
G00389482@atu.ie <G00389482@atu.ie>
2024-10-21 13:11:35 +0100

    added notification class to get id from student and set p local host on port 8081

--- src/main/java/atu/ie/enrolmentservice/Enrolment.java
+++ src/main/java/atu/ie/enrolmentservice/Enrolment.java
@@ -1,5 +1,12 @@
 package atu.ie.enrolmentservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Enrolment {
     private long enrolmentId;
     private long studentId;
